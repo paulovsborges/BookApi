1b28b7a7ad0a9ac9c3f558b23158daad5c1c29a8
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-06-22 13:54:06 -0300

    Implementing the load of data from our API to our activity.

---
+++ app/src/main/java/com/pvsb/nybooks/presentation/books/BooksViewModel.kt
@@ -0,0 +1,65 @@
+package com.pvsb.nybooks.presentation.books
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.livedata.core.ktx.R
+import com.pvsb.nybooks.data.ApiService
+import com.pvsb.nybooks.data.model.Book
+import com.pvsb.nybooks.data.response.BookBodyResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class BooksViewModel : ViewModel() {
+
+    val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
+    val viewFlipperLiveData: MutableLiveData<Pair<Int, Int?>> = MutableLiveData()
+
+    fun getBooks() {
+        ApiService.service.getBooks().enqueue(object : Callback<BookBodyResponse> {
+            override fun onResponse(
+                call: Call<BookBodyResponse>,
+                response: Response<BookBodyResponse>
+            ) {
+
+                when {
+                    response.isSuccessful -> {
+                        val books: MutableList<Book> = mutableListOf()
+
+                        response.body()?.let { bookBodyResponse ->
+                            for (result in bookBodyResponse.bookResults) {
+                                val book = result.bookDetailResponses[0].getBookModel()
+                                books.add(book)
+                            }
+                        }
+                        booksLiveData.value = books
+
+                        viewFlipperLiveData.value = Pair(VIEW_FLIPPER_BOOKS, null)
+                    }
+                    response.code() == 401 -> {
+
+                        viewFlipperLiveData.value =
+                            Pair(VIEW_FLIPPER_ERROR, com.pvsb.nybooks.R.string.books_error_401)
+                    }
+                    else -> {
+                        viewFlipperLiveData.value =
+                            Pair(
+                                VIEW_FLIPPER_ERROR,
+                                com.pvsb.nybooks.R.string.books_error_400_generic
+                            )
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<BookBodyResponse>, t: Throwable) {
+                viewFlipperLiveData.value =
+                    Pair(VIEW_FLIPPER_ERROR, com.pvsb.nybooks.R.string.books_error_500_generic)
+            }
+        })
+    }
+
+    companion object {
+        private const val VIEW_FLIPPER_BOOKS = 1
+        private const val VIEW_FLIPPER_ERROR = 2
+    }
+}
