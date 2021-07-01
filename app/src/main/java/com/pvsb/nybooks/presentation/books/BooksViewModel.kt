e527ec1730aeaff8eeac6574415713b760d3d76d
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-07-01 08:40:35 -0300

    Implementing the Coroutines scope.

--- app/src/main/java/com/pvsb/nybooks/presentation/books/BooksViewModel.kt
+++ app/src/main/java/com/pvsb/nybooks/presentation/books/BooksViewModel.kt
@@ -2,60 +2,48 @@ package com.pvsb.nybooks.presentation.books
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.livedata.core.ktx.R
-import com.pvsb.nybooks.data.ApiService
-import com.pvsb.nybooks.data.model.Book
+import androidx.lifecycle.viewModelScope
+import com.pvsb.nybooks.R
+import com.pvsb.nybooks.data.repository.Repository
 import com.pvsb.nybooks.data.response.BookBodyResponse
+import com.pvsb.nybooks.model.Book
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 
-class BooksViewModel : ViewModel() {
+class BooksViewModel(
+
+    private val repository: Repository
+
+) : ViewModel() {
 
     val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
     val viewFlipperLiveData: MutableLiveData<Pair<Int, Int?>> = MutableLiveData()
 
     fun getBooks() {
-        ApiService.service.getBooks().enqueue(object : Callback<BookBodyResponse> {
-            override fun onResponse(
-                call: Call<BookBodyResponse>,
-                response: Response<BookBodyResponse>
-            ) {
-
-                when {
-                    response.isSuccessful -> {
-                        val books: MutableList<Book> = mutableListOf()
-
-                        response.body()?.let { bookBodyResponse ->
-                            for (result in bookBodyResponse.bookResults) {
-                                val book = result.bookDetailResponses[0].getBookModel()
-                                books.add(book)
-                            }
-                        }
-                        booksLiveData.value = books
-
-                        viewFlipperLiveData.value = Pair(VIEW_FLIPPER_BOOKS, null)
-                    }
-                    response.code() == 401 -> {
-
-                        viewFlipperLiveData.value =
-                            Pair(VIEW_FLIPPER_ERROR, com.pvsb.nybooks.R.string.books_error_401)
-                    }
-                    else -> {
-                        viewFlipperLiveData.value =
-                            Pair(
-                                VIEW_FLIPPER_ERROR,
-                                com.pvsb.nybooks.R.string.books_error_400_generic
-                            )
+        viewModelScope.launch(Dispatchers.IO) {
+            runCatching {
+                repository.getBooks()
+            }.onSuccess {listBook ->
+                val books: MutableList<Book> = mutableListOf()
+                listBook.let {listBookResponse ->
+                    for (result in listBookResponse) {
+                        val book = result.bookDetailResponses[0].getBookModel()
+                        books.add(book)
                     }
                 }
+                booksLiveData.postValue(books)
+                viewFlipperLiveData.postValue(Pair(VIEW_FLIPPER_BOOKS, null))
+            }.onFailure {
+                viewFlipperLiveData.postValue(
+                    Pair(
+                        VIEW_FLIPPER_ERROR,
+                        R.string.books_error_400_generic
+                    )
+                )
             }
-
-            override fun onFailure(call: Call<BookBodyResponse>, t: Throwable) {
-                viewFlipperLiveData.value =
-                    Pair(VIEW_FLIPPER_ERROR, com.pvsb.nybooks.R.string.books_error_500_generic)
-            }
-        })
+        }
     }
 
     companion object {
