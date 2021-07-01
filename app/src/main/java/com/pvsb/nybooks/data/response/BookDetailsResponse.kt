6feae74ac568844f8ea497adbe06cc416c38729a
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-07-01 08:38:28 -0300

    refactoring the code.

--- app/src/main/java/com/pvsb/nybooks/data/response/BookDetailsResponse.kt
+++ app/src/main/java/com/pvsb/nybooks/data/response/BookDetailsResponse.kt
@@ -1,16 +1,9 @@
 package com.pvsb.nybooks.data.response
 
 import com.google.gson.annotations.SerializedName
-import com.pvsb.nybooks.data.model.Book
-import com.squareup.moshi.Json
-
+import com.pvsb.nybooks.model.Book
 
 data class BookDetailsResponse(
-
-    /**
-     * se o nome do atributo for o mesmo nome no objeto da Api, a ferramenta vai entender que estes
-     * campos são equivalente, tornando a anotação do @SerializedName(name = "") desnecessária
-     * */
 
     @SerializedName("title")
     val title: String,
